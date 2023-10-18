b91f3fb37a41f448b7af0668a02cf9111ae6849e
Ana Oliveira <125682493+aniinh4@users.noreply.github.com>
2023-10-18 15:52:25 -0300

    Add files via upload

--- jogo/jogo.js
+++ jogo/jogo.js
@@ -1,39 +1,52 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const scoreDisplay = document.getElementById('score');
+
+let score = 0;
 
 const jump = () => {
- mario.classList.add('jump');
+    mario.classList.add('jump');
 
- setTimeout(() => {
- mario.classList.remove('jump');
- }, 500);
+    setTimeout(() => {
+        mario.classList.remove('jump');
+        
+        // Obtém as posições do Mario e do cano
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = mario.offsetLeft;
+
+        // Verifica se o Mario passou pelo cano com sucesso (ajuste as condições conforme necessário)
+        if (marioPosition => pipePosition && marioPosition <= pipePosition + pipe.offsetWidth) {
+            score++;
+            scoreDisplay.textContent = `Pontuação: ${score}`;
+        }
+    }, 500);
 }
 
 const loop = setInterval(() => {
 
- console.log('loop')
+    console.log('loop');
 
- const pipePosition = pipe.offsetLeft;
- const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
- console.log(marioPosition);
- 
- if (pipePosition < 120  &&  pipePosition > 0 && marioPosition < 80) {
- 
- pipe.style.animation = 'none';
- pipe.style.left = `${pipePosition}px`;
+    console.log(marioPosition);
 
- mario.style.animation = 'none';
- mario.style.bottom = `${marioPosition}px`;
+    if (pipePosition < 120  &&  pipePosition > 0 && marioPosition < 80) {
 
- mario.src = 'game-over.png';
- mario.style.width = '75px'
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
 
- mario.style.marginLeft = '50px'
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
 
- clearInterval(loop);
- }
+        mario.src = 'game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
 
 }, 10);
 
 document.addEventListener('keydown', jump);
+
